d10e0c1e11dcae8ea8793e5a69be95f68ed39b2b
claus <claus@test.de>
2020-05-17 22:25:46 +0200

    Doku CDI

--- claus-project/src/main/java/de/claus/cdi/CDIDemo.java
+++ claus-project/src/main/java/de/claus/cdi/CDIDemo.java
@@ -4,6 +4,9 @@ import java.util.ServiceLoader;
 
 public class CDIDemo {
 	public static void main(String[] args) {
+		/**
+		 * Der Partnerservice befindet sich unter META-INF/services als Datei ohne Endung
+		 */
 		ServiceLoader<PartnerService> partnerServices = ServiceLoader.load( PartnerService.class );
 		for ( PartnerService partner : partnerServices )
 		  System.out.println( partner.getClass() + " : " + partner.hello() );
